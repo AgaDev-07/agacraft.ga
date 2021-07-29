c7557bbd65df75ddc6ad1b1fe5eff3737ee7bab7
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2021-07-28 21:38:23 -0500

    initial-commit

---
+++ js/seo/ChoesinCraft.js
@@ -0,0 +1,10 @@
+var pass = prompt("ingresa la contraseña");
+
+if(pass=="SeoMC99" || pass=="seomc99" || pass=="elver galarga"){
+  location.href="https://www.mediafire.com/file/du063qv9gtiosxo/ChoesinCraft.mcaddon/file";
+}
+else{
+  alert("la contraseña es incorrecta porfavor verifica que la estes ingresando correctamente");
+  location.href="/seo/ChoesinCraft";
+}
+//
