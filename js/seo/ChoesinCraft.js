101f7da204feeae1952016117e41851b52189222
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2021-08-16 23:09:25 -0500

    Add files via upload

--- js/seo/ChoesinCraft.js
+++ js/seo/ChoesinCraft.js
@@ -1,10 +1,11 @@
-var pass = prompt("ingresa la contraseña");
+const cuentas = require('/data/cuentas.json');
+var pass = prompt("SeoMC99"/*"Ingresa la contraseña"*/);
+var correcto = cuentas.seomc99.pass;
 
-if(pass=="SeoMC99" || pass=="seomc99" || pass=="elver galarga"){
+if(pass == correcto[0]||pass == correcto[1]/*pass=="SeoMC99" || pass=="seomc99" || pass=="elver galarga"*/){
   location.href="https://www.mediafire.com/file/du063qv9gtiosxo/ChoesinCraft.mcaddon/file";
 }
 else{
   alert("la contraseña es incorrecta porfavor verifica que la estes ingresando correctamente");
   location.href="/seo/ChoesinCraft";
 }
-//
