c8058bc22f59b0120c30a2926b33efed1fc8eedc
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2021-08-25 23:56:06 -0500

    Add files via upload

---
+++ js/esconder.js
@@ -0,0 +1,10 @@
+function showDiv1() {
+  document.getElementById('div1').style.display = '';
+  document.getElementById('bo1').style.display = 'none';
+  document.getElementById('bo2').style.display = '';
+}
+function noShowDiv1() {
+  document.getElementById('div1').style.display = 'none';
+  document.getElementById('bo1').style.display = '';
+  document.getElementById('bo2').style.display = 'none';
+}
