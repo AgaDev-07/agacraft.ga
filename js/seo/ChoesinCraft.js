21c990cecdb2e962cc782b4d29a37a4c671b8480
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2021-09-30 10:25:24 -0500

    Update ChoesinCraft.js

--- js/seo/ChoesinCraft.js
+++ js/seo/ChoesinCraft.js
@@ -12,11 +12,15 @@ function validar (){
   const parrafo = document.getElementById("no")
   const user = document.getElementById("user").value;
   const pass = document.getElementById("pass").value;
-  if(user=="SeoMC99" && pass=="@NJM13+PJS13**"){
+  const validate = /^@([^A-MO-Z][^A-IK-Z][^A-LN-Z][1][3])([+])([^A-OQ-Z][^A-IK-Z][^A-RT-Z][1][3])([*][*])$/;
+  const contra = validate.test(pass);
+  if(user=="SeoMC99" && contra==true){
+    console.log(contra);
     alert("usuario y contraseña validos");
     document.getElementById('login').style.display = 'none';
     document.getElementById('descargar').style.display = '';
   }else {
+    console.log(contra);
     if(user=="SeoMC99") {
       parrafo.innerHTML = `${user} tu contraseña es incorresta`;
     }else {
