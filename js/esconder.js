ab2fa534b5ebdf435cdf2b4fc9afff8c02e24b6f
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2021-10-05 15:12:21 -0500

    Update esconder.js

--- js/esconder.js
+++ js/esconder.js
@@ -3,21 +3,25 @@ function showDiv1() {
   document.getElementById('div2').style.display = 'none';
   document.getElementById('bo1').style.display = 'none';
   document.getElementById('bo2').style.display = '';
+  document.getElementById('bo3').style.display = 'none';
 }
 function noShowDiv1() {
   document.getElementById('div1').style.display = 'none';
   document.getElementById('bo1').style.display = '';
   document.getElementById('bo2').style.display = 'none';
+  document.getElementById('bo4').style.display = 'none';
 }
 
 function showDiv2() {
   document.getElementById('div1').style.display = 'none';
   document.getElementById('div2').style.display = '';
+  document.getElementById('bo1').style.display = 'none';
+  document.getElementById('bo2').style.display = '';
   document.getElementById('bo3').style.display = 'none';
-  document.getElementById('bo4').style.display = '';
 }
 function noShowDiv2() {
   document.getElementById('div2').style.display = 'none';
-  document.getElementById('bo3').style.display = '';
+  document.getElementById('bo1').style.display = '';
+  document.getElementById('bo2').style.display = 'none';
   document.getElementById('bo4').style.display = 'none';
 }
