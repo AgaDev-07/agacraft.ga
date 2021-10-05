9ae82e39bb5139d800d3eaf9b5893574572bde68
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2021-10-05 16:02:13 -0500

    Update esconder.js

--- js/esconder.js
+++ js/esconder.js
@@ -17,10 +17,10 @@ function noShowDiv1() {
 function showDiv2() {
   document.getElementById('div1').style.display = 'none';
   document.getElementById('div2').style.display = '';
-  document.getElementById('bo1').style.display = 'none';
-  document.getElementById('bo2').style.display = '';
-  document.getElementById('bo3').style.display = '';
-  document.getElementById('bo4').style.display = 'none';
+  document.getElementById('bo1').style.display = '';
+  document.getElementById('bo2').style.display = 'none';
+  document.getElementById('bo3').style.display = 'none';
+  document.getElementById('bo4').style.display = '';
 }
 function noShowDiv2() {
   document.getElementById('div2').style.display = 'none';
