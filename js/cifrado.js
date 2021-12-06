91761836e8541812034d813d539c1332dc41048f
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2021-12-06 05:33:13 +0000

    cifrado prueba 3

--- js/cifrado.js
+++ js/cifrado.js
@@ -2,11 +2,13 @@ const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 function cifrar(texto, desplazamiento){
   if(!texto)return '';
+  texto = texto.toUpperCase()
   desplazamiento = (desplazamiento % 26 + 26)%26;
-  return texto.replace(/[a-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26])
+  return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26])
 }
 function deCifrar(texto, desplazamiento){
   if(!texto)return '';
+  texto = texto.toUpperCase()
   desplazamiento = (desplazamiento % 26 - 26)%26;
-  return texto.replace(/[a-Z]/ig, c=> letras[(letras.indexOf(c)-desplazamiento)%26])
+  return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-desplazamiento)%26])
 }
