2de1ea4dfa54f33afa59e26127302ca6688cdf5d
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2021-12-06 06:26:22 +0000

    cifrado 5.1

--- js/seo/ChoesinCraft.js
+++ js/seo/ChoesinCraft.js
@@ -1,31 +1,31 @@
 import { cifrar, deCifrar } from '/js/cifrado.js'
 
 function pass() {
-document.getElementById('btn1').style.display = 'none';
-document.getElementById('btn2').style.display = '';
-document.getElementById('pass').type='text';
+  document.getElementById('btn1').style.display = 'none';
+  document.getElementById('btn2').style.display = '';
+  document.getElementById('pass').type = 'text';
 }
 function passNo() {
   document.getElementById('btn1').style.display = '';
   document.getElementById('btn2').style.display = 'none';
-  document.getElementById('pass').type='password';
+  document.getElementById('pass').type = 'password';
 }
-function validar (){
+function validar() {
   const parrafo = document.getElementById("no")
   const user = document.getElementById("user").value;
   const pass = document.getElementById("pass").value;
   const validate = /^@([^0-9A-MO-Za-z][^0-9A-IK-Za-z][^0-9A-LN-Za-z][1][3])([+])([^0-9A-OQ-Za-z][^0-9A-IK-Za-z][^0-9A-RT-Za-z][1][3])([*][*])$/;
   const contra = validate.test(pass);
-  if(user=="SeoMC99" && contra==true){
+  if (user == "SeoMC99" && contra == true) {
     console.log(contra);
     document.getElementById('login').style.display = 'none';
     document.getElementById('descargar').style.display = '';
-    document.getElementById('load').id='download';
-  }else {
+    document.getElementById('load').id = 'download';
+  } else {
     console.log(contra);
-    if(user=="SeoMC99") {
+    if (user == "SeoMC99") {
       parrafo.innerHTML = `${user} tu contraseña es incorresta`;
-    }else {
+    } else {
       parrafo.innerHTML = `"${user}" invalido`;
     }
   }
@@ -34,10 +34,12 @@ function validar (){
 
   console.log(cifrado, deCifrado)
 }
-document.addEventListener('keydown', function(tecla){
+document.addEventListener('keydown', function (tecla) {
   var codigoTecla = tecla.keyCode;
-  if(codigoTecla == 13){
+  if (codigoTecla == 13) {
     window.location.href = document.links.download.href;
     validar();
   }
 });
+
+export { pass, passNo, validar }
