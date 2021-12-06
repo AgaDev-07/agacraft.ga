71de1342136b39a76c92e963bf199fcaffea19c8
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2021-12-06 05:24:30 +0000

    cifrado prueba 2

--- js/seo/ChoesinCraft.js
+++ js/seo/ChoesinCraft.js
@@ -1,40 +1,49 @@
+function importarScript(nombre, callback) {
+  var s = document.createElement("script");
+  s.onload = callback;
+  s.src = nombre;
+  document.querySelector("head").appendChild(s);
+}
+
 function pass() {
-document.getElementById('btn1').style.display = 'none';
-document.getElementById('btn2').style.display = '';
-document.getElementById('pass').type='text';
+  document.getElementById('btn1').style.display = 'none';
+  document.getElementById('btn2').style.display = '';
+  document.getElementById('pass').type = 'text';
 }
 function passNo() {
   document.getElementById('btn1').style.display = '';
   document.getElementById('btn2').style.display = 'none';
-  document.getElementById('pass').type='password';
+  document.getElementById('pass').type = 'password';
 }
-function validar (){
+function validar() {
   const parrafo = document.getElementById("no")
   const user = document.getElementById("user").value;
   const pass = document.getElementById("pass").value;
   const validate = /^@([^0-9A-MO-Za-z][^0-9A-IK-Za-z][^0-9A-LN-Za-z][1][3])([+])([^0-9A-OQ-Za-z][^0-9A-IK-Za-z][^0-9A-RT-Za-z][1][3])([*][*])$/;
   const contra = validate.test(pass);
-  if(user=="SeoMC99" && contra==true){
+  if (user == "SeoMC99" && contra == true) {
     console.log(contra);
     document.getElementById('login').style.display = 'none';
     document.getElementById('descargar').style.display = '';
-    document.getElementById('load').id='download';
-  }else {
+    document.getElementById('load').id = 'download';
+  } else {
     console.log(contra);
-    if(user=="SeoMC99") {
+    if (user == "SeoMC99") {
       parrafo.innerHTML = `${user} tu contraseña es incorresta`;
-    }else {
+    } else {
       parrafo.innerHTML = `"${user}" invalido`;
     }
   }
-  let cifrado = cifrar(pass, 7);
-  let deCifrado = deCifrar(cifrado, 7);
-
-  console.log(cifrado, deCifrado)
+  importarScript("../cifrado.js", () => {
+    let cifrado = cifrar(pass, 7);
+    let deCifrado = deCifrar(cifrado, 7);
+    
+    console.log(cifrado, deCifrado)
+  });
 }
-document.addEventListener('keydown', function(tecla){
+document.addEventListener('keydown', function (tecla) {
   var codigoTecla = tecla.keyCode;
-  if(codigoTecla == 13){
+  if (codigoTecla == 13) {
     window.location.href = document.links.download.href;
     validar();
   }
