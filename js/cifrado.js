789f912394787fb0a808dec8e9156ef2abc9e87c
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2021-12-06 05:58:08 +0000

    Cifrado prueba 4

--- js/cifrado.js
+++ js/cifrado.js
@@ -1,14 +1,16 @@
 const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-export function cifrar(texto, desplazamiento){
-  if(!texto)return '';
+function cifrar(texto, desplazamiento) {
+  if (!texto) return '';
   texto = texto.toUpperCase()
-  desplazamiento = (desplazamiento % 26 + 26)%26;
-  return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26])
+  desplazamiento = (desplazamiento % 26 + 26) % 26;
+  return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) + desplazamiento) % 26])
 }
-export function deCifrar(texto, desplazamiento){
-  if(!texto)return '';
+function deCifrar(texto, desplazamiento) {
+  if (!texto) return '';
   texto = texto.toUpperCase()
-  desplazamiento = (desplazamiento % 26 - 26)%26;
-  return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-desplazamiento)%26])
+  desplazamiento = (desplazamiento % 26 - 26) % 26;
+  return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) - desplazamiento) % 26])
 }
+
+export { cifrar, deCifrar }
