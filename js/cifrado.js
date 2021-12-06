fcc32b04208d75f2e95e0bb7bbf781b371684d24
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2021-12-06 05:37:42 +0000

    cifrado prueba 3 (se me olvido exportar :/)

--- js/cifrado.js
+++ js/cifrado.js
@@ -1,12 +1,12 @@
 const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-function cifrar(texto, desplazamiento){
+export function cifrar(texto, desplazamiento){
   if(!texto)return '';
   texto = texto.toUpperCase()
   desplazamiento = (desplazamiento % 26 + 26)%26;
   return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26])
 }
-function deCifrar(texto, desplazamiento){
+export function deCifrar(texto, desplazamiento){
   if(!texto)return '';
   texto = texto.toUpperCase()
   desplazamiento = (desplazamiento % 26 - 26)%26;
