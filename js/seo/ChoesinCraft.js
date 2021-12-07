749ec2faf89f573069b7aa368bf42d928dea3a11
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2021-12-07 16:20:52 -0600

    Update ChoesinCraft.js

--- js/seo/ChoesinCraft.js
+++ js/seo/ChoesinCraft.js
@@ -13,8 +13,8 @@ function validar() {
   const user = document.getElementById("user").value;
   const pass = document.getElementById("pass").value;
   const validate = /^@([^0-9A-MO-Za-z][^0-9A-IK-Za-z][^0-9A-LN-Za-z][1][3])([+])([^0-9A-OQ-Za-z][^0-9A-IK-Za-z][^0-9A-RT-Za-z][1][3])([*][*])$/;
-const mseo= process.env['seo']
-cconst contra = validate.test(pass);
+const mseo= process.env['seo'] || 5
+const contra = validate.test(pass);
   if (user == "SeoMC99" && contra == true) {
     console.log(contra);
     document.getElementById('login').style.display = 'none';
