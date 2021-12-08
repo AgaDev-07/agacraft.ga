5f02133c046d479e3b0fe752f90101b28ff8348c
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2021-12-08 01:21:12 -0600

    Update ChoesinCraft.js

--- js/seo/ChoesinCraft.js
+++ js/seo/ChoesinCraft.js
@@ -1,3 +1,18 @@
+const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function cifrar(texto, desplazamiento) {
+  if (!texto) return '';
+  texto = texto.toUpperCase()
+  desplazamiento = (desplazamiento % 26 + 26) % 26;
+  return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) + desplazamiento) % 26])
+}
+function deCifrar(texto, desplazamiento) {
+  if (!texto) return '';
+  texto = texto.toUpperCase()
+  desplazamiento = (desplazamiento % 26 - 26) % 26;
+  return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) - desplazamiento) % 26])
+}
+
 function pass() {
   document.getElementById('btn1').style.display = 'none';
   document.getElementById('btn2').style.display = '';
