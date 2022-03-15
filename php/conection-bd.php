270e27e408c15bbc0d44f2d5880f0da7aa415c69
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2022-03-15 19:19:15 +0000

    php3.2

--- php/conection-bd.php
+++ php/conection-bd.php
@@ -1,5 +1,5 @@
 <?php
-  $connection = mysli_connect('bolcn8rt1xey0wkabo0k-mysql.services.clever-cloud.com', 'ugjlmttvbbo0x4f7', 'Uq6ELB8ydnwox8M8P0pN', 'web') or die(mysql_error($mysqli));
+  $connection = mysli_connect('bolcn8rt1xey0wkabo0k-mysql.services.clever-cloud.com', 'ugjlmttvbbo0x4f7', 'Uq6ELB8ydnwox8M8P0pN', 'bolcn8rt1xey0wkabo0k') or die(mysql_error($mysqli));
 
   insert($connection)
 
@@ -7,7 +7,7 @@
     $user = $_POST['user'];
     $password = $_POST['password'];
 
-    $consulta = "INSERT INTO persona(user, password) VALUES ('$user', '$password')";
+    $consulta = "INSERT INTO bolcn8rt1xey0wkabo0k(user, password) VALUES ('$user', '$password')";
     mysqli_query($connection, $consulta);
     mysqli_close($connection);
   }
