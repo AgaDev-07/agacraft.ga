155e0b129527dbe66c8e649f6a6a7371ea9662cb
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2022-03-15 18:54:08 +0000

    php2.1

--- php/conection-bd.php
+++ php/conection-bd.php
@@ -1,5 +1,5 @@
 <?php
-  $connection = mysli_connect('bolcn8rt1xey0wkabo0k-mysql.services.clever-cloud.com', 'ugjlmttvbbo0x4f7', 'ugjlmttvbbo0x4f7') or die(mysql_error($mysqli));
+  $connection = mysli_connect('bolcn8rt1xey0wkabo0k-mysql.services.clever-cloud.com', 'ugjlmttvbbo0x4f7', 'ugjlmttvbbo0x4f7', 'web') or die(mysql_error($mysqli));
 
   insert($connection)
 
