4024d1bdbff357c3ea526f784f642276193cbeb9
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2022-04-12 17:55:01 +0000

    new php

--- php/conection-bd.php
+++ php/conection-bd.php
@@ -1,18 +1,5 @@
 <?php
-  $host = 'bolcn8rt1xey0wkabo0k-mysql.services.clever-cloud.com'
-  $user = 'ugjlmttvbbo0x4f7'
-  $password = 'Uq6ELB8ydnwox8M8P0pN'
-  $data_base = 'bolcn8rt1xey0wkabo0k'
-  $connection = mysqli_connect($host, $user, $password, $data_base) or die(mysql_error($mysqli));
-
-  insert($connection)
-
-  function insert($connection){
-    $user = $_POST['user'];
-    $password = $_POST['password'];
-
-    $consulta = "INSERT INTO $data_base(user, password) VALUES ('$user', '$password')";
-    mysqli_query($connection, $consulta);
-    mysqli_close($connection);
-  }
+  require 'vendor/autoload.php'
+  $client = new MongoDB\Client( 'mongodb+srv://connector:ZhZAtNrfFNNqUjrG@cluster0.9qalr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+  $db = $client->test;
 ?>
