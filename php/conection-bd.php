49a5d7b0774e2d4e717f56efecd6c067ef4cbe48
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2022-07-15 22:25:27 -0500

    Delete php directory

--- php/conection-bd.php
+++
@@ -1,5 +0,0 @@
-<?php
-  require 'vendor/autoload.php'
-  $client = new MongoDB\Client( 'mongodb+srv://connector:ZhZAtNrfFNNqUjrG@cluster0.9qalr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-  $db = $client->test;
-?>
