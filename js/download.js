48da6c04f08878cb12da8c3cac986e7b96c8417e
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2022-07-17 02:10:50 -0500

    Delete js directory

--- js/download.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('keydown', function(tecla){
-  if(tecla.keyCode == 13){
-    try {
-      download()
-    } catch (error) {
-      window.location.href = document.links.download.href
-    }
-  }
-});
