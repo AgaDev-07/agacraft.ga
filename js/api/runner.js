2f8be60e40cbe0f8f0b83f330fb701ce05e598af
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2022-07-25 00:25:31 -0500

    Add api for html with js

---
+++ js/api/runner.js
@@ -0,0 +1,105 @@
+getDefault()
+function getDefault() {
+  const $ = e => document.querySelector(e);
+  let interval;
+  let fn = () => FN();
+  let FN = () => {
+    $('nav').innerHTML += nav;
+    $('footer').innerHTML += footer;
+    $('header').innerHTML += wave;
+    FN = () => {
+      clearInterval(interval);
+      interval = null;
+    };
+  };
+  interval = setInterval(fn, 100);
+}
+
+/**
+ * @param {'addons'|'seo'|'texturas'} page
+ * @returns {String[]}
+ */
+function getApi(page) {
+  return {
+    addons: [
+      'Iron-Chest',
+      'Waila',
+      'Home',
+      'Fundas-de-equipamiento',
+      'Chisel',
+      'Variantes-de-madera',
+    ],
+    texturas: ['Escudo-secundario'],
+    seo: ['Home', 'ChoesinCraft'],
+  }[page];
+}
+function addApiTA(page) {
+  let interval;
+  let Api = () => {
+    document.querySelector('.galeria-port').innerHTML += getApi(page)
+      .map(
+        name =>
+          `<div class="imagen-port">
+        <img src="/img/${page}/${name}/icono.png" alt="" />
+        <a href="/${page}/${name}">
+          <div class="hover-galeria">
+            <img src="/img/click.png" alt="" />
+            <p>${name.replace('-', ' ')}</p>
+          </div>
+        </a>
+      </div>`
+      )
+      .join('');
+    Api = () => {
+      clearInterval(interval);
+      interval = null;
+    };
+  };
+  function fn() {
+    return Api();
+  }
+
+  interval = setInterval(fn, 100);
+}
+
+function addApiIndex() {
+  let interval;
+  let Api = () => {
+    document.querySelector('main').innerHTML += [
+      { title: 'Addon mas reciente', page: 'addons' },
+      { title: 'Textura mas reciente', page: 'texturas' },
+      { title: 'Contenido mas reciente para SeoMC99', page: 'seo' },
+    ]
+      .map(({ title, page }, i) => {
+        let name = getApi(page)[0];
+        let section = 2;
+        if (Math.round(i / 2) * 2 == i) section = 1;
+        return `<section class="section-${section}">
+            <div class="contenedor">
+              <h2 class="titulo">${title}</h2>
+              <div class="galeria-port">
+                <div class="imagen-port">
+                  <img src="/img/${page}/${name}/icono.png" alt="">
+                  <a href="/${page}/${name}">
+                    <div class="hover-galeria">
+                      <img src="/img/click.png" alt="">
+                      <p>${name.replace('-', ' ')}</p>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </section>`;
+      })
+      .join('');
+    Api = () => {
+      clearInterval(interval);
+      interval = null;
+    };
+  };
+  function fn() {
+    return Api();
+  }
+
+  interval = setInterval(fn, 100);
+}
