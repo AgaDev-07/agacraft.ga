479cefe05ccc207ffe22feff10914fe51c48a7a2
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2022-07-26 09:29:36 -0500

    Add files via upload

--- js/seo/login.js
+++ js/seo/login.js
@@ -1,14 +1,28 @@
-﻿function isUrl(Url) {
-  let url = document.URL;
-  return url.endsWith(Url) || url.endsWith(Url + '.html');
+﻿﻿const letters =
+  'ABCabcDEFdefGHIghiJKLjklMNOmnoPQRpqrSTUstuVWXvwxYZyz0123456789-:./@+*?¿()[]{}<>#$&!|°';
+const reg = /[A-Za-z0-9-@+*?¿()[\]{}<>#$&!|°:/.]/g
+const numberLetters = letters.length;
+function cifrar(texto, desplazamiento = 1) {
+  if (!texto) return '';
+  desplazamiento = ((desplazamiento % numberLetters) + numberLetters) % numberLetters;
+  return texto.replace(reg,
+    c => letters[(letters.indexOf(c) + desplazamiento) % numberLetters]
+  );
 }
-function getLink() {
-  if (isUrl('ChoesinCraft')) return 'ChoesinCraft';
-  if (isUrl('Home')) return 'Home';
-  alert('no se pudo encontrar el link que nesesitas');
-  return '#';
+function isUrl(Url){
+  let url=document.URL;
+  return url.endsWith(Url) || url.endsWith(Url+'.html')
+}
+function getLink(){
+  if(isUrl('ChoesinCraft')) return cifrar('frrnq688uuu7kcbG|DGPc7AMk8DGJc8bSx1yot4erGMqVM8!fMcqGl!P|Dr7kA|bbMl8DGJc', 5);
+  if(isUrl('Home')) return cifrar('frrnq688uuu7kcbG|DGPc7AMk8DGJc8xflPl°Aoniyebnw8FMkc_qcM7kA|bbMl8DGJc', 5)
+  alert('no se pudo encontrar el link que nesesitas')
+  return '#'
 }
 
+const counts = {
+  'WiSQd**': '0fbesu1hbksu22',
+};
 function pass() {
   document.getElementById('btn').value = 'o';
   document.getElementById('btn').onclick = () => passNo();
@@ -19,54 +33,13 @@ function passNo() {
   document.getElementById('btn').value = '-';
   document.getElementById('pass').type = 'password';
 }
-function StringToURL(string) {
-  let URL = "% !*'();:@&=+$/?#[]".split(
-    (_, i) =>
-      [
-        '%25',
-        '%20',
-        '%21',
-        '%2a',
-        '%27',
-        '%28',
-        '%29',
-        '%3B',
-        '%3A',
-        '%40',
-        '%26',
-        '%3D',
-        '%2B',
-        '%24',
-        '%2F',
-        '%3F',
-        '%23',
-        '%5B',
-        '%5D',
-      ][i]
-  );
-  return URL;
-}
 let download = () => {
-  fetch('https://aga-db.herokuapp.com/', {
-    method: 'GET',
-    headers: {
-      Accept: '*/*',
-      'User-Agent': 'Aga Web (https://agacraft07.github.io)',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      user: document.getElementById('user').value,
-      password: document.getElementById('pass').value,
-      page: 'seo',
-      addon: getLink(),
-    }),
-  })
-    .then(response => response.text())
-    .then(function (data) {
-      if (data == '#') return;
-      document.getElementById('login').style.display = 'none';
-      document.getElementById('descargar').style.display = '';
-      document.getElementById('download').href = data;
-      download = undefined;
-    });
+  const user = document.getElementById('user').value;
+  const contra = cifrar(counts[cifrar(user, 7)] || 'a', 99);
+  if (contra === document.getElementById('pass').value) {
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('descargar').style.display = '';
+    document.getElementById('download').href = getLink();
+    download = undefined;
+  } else document.getElementById('no').innerHTML = `usuario o contraseña incorrecta`;
 };
