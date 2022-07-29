72443d00557be0ea36fd63004bef984cfb1ef3b4
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2022-07-29 01:05:10 -0500

    Update data.js

--- js/JunMC13/data.js
+++ js/JunMC13/data.js
@@ -3,7 +3,7 @@ const counts = {
   EkedVeJx: '0xL}JV#v'
 };
 const url = {
-  'Textues': 'frrnq688uuu7kcbG|DGPc7AMk8DGJc8tfJS22lhzwrwo4°8RcVrSPcq7kA|bbMl8DGJc',
+  'Texturas': 'frrnq688uuu7kcbG|DGPc7AMk8DGJc8tfJS22lhzwrwo4°8RcVrSPcq7kA|bbMl8DGJc',
   'Home': 'frrnq688uuu7kcbG|DGPc7AMk8DGJc8xflPl°Aoniyebnw8FMkc_qcM7kA|bbMl8DGJc',
   'ChoesinCraft':'frrnq688uuu7kcbG|DGPc7AMk8DGJc8bSx1yot4erGMqVM8!fMcqGl!P|Dr7kA|bbMl8DGJc'
 }
