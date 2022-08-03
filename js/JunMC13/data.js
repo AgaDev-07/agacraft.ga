204db74c3095ee7152640a1e663158eeccc37347
AdrianCraft07 <proadrian684@gmail.com>
2022-08-03 18:14:45 +1000

    update login

--- js/JunMC13/data.js
+++
@@ -1,9 +0,0 @@
-const counts = {
-  'NYrQd8-': 'Yfbe<su1hbksuY',
-  EkedVeJx: '0xL}JV#v'
-};
-const url = {
-  'Texturas': 'frrnq688uuu7kcbG|DGPc7AMk8DGJc8tfJS22lhzwrwo4°8RcVrSPcq7kA|bbMl8DGJc',
-  'Home': 'frrnq688uuu7kcbG|DGPc7AMk8DGJc8xflPl°Aoniyebnw8FMkc_qcM7kA|bbMl8DGJc',
-  'ChoesinCraft':'frrnq688uuu7kcbG|DGPc7AMk8DGJc8bSx1yot4erGMqVM8!fMcqGl!P|Dr7kA|bbMl8DGJc'
-}
--- js/login.js
+++ js/login.js
@@ -1,31 +1,26 @@
-const letters =
-  'ABCabcDEFdefGHIghiJKLjklMNOmnoPQRpqrSTUstuVWXvwxYZyz0123456789-:./@+*?¿()[]{}<>#$&!|°';
-const reg = /[A-Za-z0-9-@+*?¿()[\]{}<>#$&!|°:/.]/g
-const numberLetters = letters.length;
-function cifrar(texto, desplazamiento = 1) {
-  if (!texto) return '';
-  desplazamiento = ((desplazamiento % numberLetters) + numberLetters) % numberLetters;
-  return texto.replace(reg,
-    c => letters[(letters.indexOf(c) + desplazamiento) % numberLetters]
-  );
-}
-function isUrl(Url){
-  let url=document.URL;
-  return url.endsWith(Url) || url.endsWith(Url+'.html')
-}
-function getLink(){
-  const keys = Object.keys(url);
-  return keys.map(Url=>isUrl(Url)?url[Url]:'').join('');
-  alert('no se pudo encontrar el link que nesesitas')
-  return '#'
-}
+const $ = e => document.querySelector(e);
+const $$ = e => document.querySelectorAll(e);
+
 function pass() {
-  document.getElementById('btn').value = 'o';
-  document.getElementById('btn').onclick = () => passNo();
-  document.getElementById('pass').type = 'text';
+  if ($("#pass").type === "password") {
+    $("#pass").type = "text";
+    btn.value = 'o'
+  } else {
+    $("#pass").type = "password";
+    $("btnpass").value = '-'
+  }
 }
-function passNo() {
-  document.getElementById('btn').onclick = () => pass();
-  document.getElementById('btn').value = '-';
-  document.getElementById('pass').type = 'password';
+function download(page, addon) {
+  let user = $("#user").value;
+  let password =$("#pass").value;
+  if (user === "") {
+    $("#no").innerHTML = "Ingrese un usuario";
+  } else if (password === "") {
+    $("#no").innerHTML = "Ingrese una contraseña";
+  } else {
+    window.location.href = `https://aga-db.herokuapp.com/${page}/${addon}?user=${stringToUrl(user)}&password=${stringToUrl(password)}`;
+  }
+}
+function stringToUrl(str) {
+  return str.replaceAll(' ', "%20").replaceAll('&', "%26").replaceAll('?', "%3F").replaceAll('=', "%3D").replaceAll('+', "%2B").replaceAll('#', "%23").replaceAll('/', "%2F").replaceAll('\\', "%5C").replaceAll('\'', "%27").replaceAll('\"', "%22").replaceAll('<', "%3C").replaceAll('>', "%3E").replaceAll('(', "%28").replaceAll(')', "%29").replaceAll('[', "%5B").replaceAll(']', "%5D").replaceAll('{', "%7B").replaceAll('}', "%7D").replaceAll('^', "%5E").replaceAll('$', "%24").replaceAll('!', "%21").replaceAll('*', "%2A").replaceAll('|', "%7C").replaceAll('~', "%7E").replaceAll('`', "%60");
 }
