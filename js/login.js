3224d86557528e5503d7c3e3e834d6f2a2ccf7c2
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2022-08-12 22:14:11 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -15,23 +15,9 @@ function download(page, addon) {
   let password = $("#pass").value;
   if (user === "") $("#no").innerHTML = "Ingrese un usuario";
   else if (password === "") $("#no").innerHTML = "Ingrese una contraseña";
-  else {
-    try {
-      const db = new XMLHttpRequest();
-      db.open("GET", `https://aga-db.herokuapp.com/${page}/${addon}?user=${stringToUrl(user)}&password=${stringToUrl(password)}`);
-      db.send(null);
-      let data = JSON.parse(db.responseText)
-      if (data.VALID) {
-        $('#login').style.display = "none";
-        $('.container').style.display = "block";
-        $('.login-download').href = data.URL
-        $('.login-download').id = 'download'
-        throw new Error('VALID');
-      } else $("#no").innerHTML = 'Usuario o contraseña incorrectos';
-    } catch (error) {
-      console.error(error)
-    }
-  }
+  else 
+    window.location.href = `https://aga-db.herokuapp.com/${page}/${addon}?user=${stringToUrl(user)}&password=${stringToUrl(password)}`;
+  
 }
 function stringToUrl(str) {
   return str.replaceAll(' ', "%20").replaceAll('&', "%26").replaceAll('?', "%3F").replaceAll('=', "%3D").replaceAll('+', "%2B").replaceAll('#', "%23").replaceAll('/', "%2F").replaceAll('\\', "%5C").replaceAll('\'', "%27").replaceAll('\"', "%22").replaceAll('<', "%3C").replaceAll('>', "%3E").replaceAll('(', "%28").replaceAll(')', "%29").replaceAll('[', "%5B").replaceAll(']', "%5D").replaceAll('{', "%7B").replaceAll('}', "%7D").replaceAll('^', "%5E").replaceAll('$', "%24").replaceAll('!', "%21").replaceAll('*', "%2A").replaceAll('|', "%7C").replaceAll('~', "%7E").replaceAll('`', "%60");
