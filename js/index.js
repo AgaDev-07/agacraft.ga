fd850560098c937aec6b001a8266f1055d45c3d0
AdrianCraft07 <88064157+AdrianCraft07@users.noreply.github.com>
2022-08-19 00:02:34 -0500

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,2 @@
+<script>(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })()</script>
+<script src="/js/test/login.js"></script>
