2398b545354a10f03c0cfa59dac23ac2e9df8f5f
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2022-09-14 00:07:53 -0500

    Update data.js

--- js/api/data.js
+++ js/api/data.js
@@ -6,41 +6,6 @@ async function getApi() {
     .then(v => v.text())
     .then(JSON.parse);
 }
-
-const nav = `
-<a href="/">Inicio</a>
-<a href="/addons">Addons</a>
-<a href="/texturas">Texturas</a>
-<a href="/JunMC13">JunMC13</a>`;
-const footer = `
-<div class="contenedor-footer">
-  <div class="content-foo">
-    <h4>Discord</h4>
-    <a class="link-s"  target="_blank" href="https://discord.gg/9BqzsNpvpS">AgaCraft</a>
-  </div>
-  <div class="content-foo">
-    <h4>Correo electronico</h4>
-    <a
-      target="_blank"
-      class="link-s"
-      href="mailto:agacraft.addons@gmail.com?body=Hola AdrianCraft,
-    Me comunico contigo para decirte"
-      >agacraft.addons@gmail.com</a
-    >
-  </div>
-  <div class="content-foo">
-    <h4>YouTube</h4>
-    <a class="link-s" target="_blank" href="https://youtube.com/c/Adriancraft07">AdrianCraft</a>
-  </div>
-</div>
-<h2 class="titulo-final">&copy; AdrianCraft</h2>`;
-const wave = `
-<div class="wave">
-  <svg viewBox="0 0 500 150" preserveAspectRatio="none">
-    <path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"></path>
-  </svg>
-</div>`;
-
 const vue = {
   nav: [
     ['/', 'Inicio'],
