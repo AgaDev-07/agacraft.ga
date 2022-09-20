dac32951a1a39d259e5bba5e5e204b94593c0c6c
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2022-09-19 19:37:00 -0500

    Update show.js

--- js/api/show.js
+++ js/api/show.js
@@ -1,3 +1,37 @@
+function urlToString(url) {
+  return (url || 'null')
+    .replaceAll('%20', ' ')
+    .replaceAll('%21', '!')
+    .replaceAll('%22', '"')
+    .replaceAll('%23', '#')
+    .replaceAll('%24', '$')
+
+    .replaceAll('%26', '&')
+    .replaceAll('%27', "'")
+    .replaceAll('%28', '(')
+    .replaceAll('%29', ')')
+    .replaceAll('%2A', '*')
+    .replaceAll('%2B', '+')
+
+    .replaceAll('%2F', '/')
+
+    .replaceAll('%3C', '<')
+    .replaceAll('%3D', '=')
+    .replaceAll('%3E', '>')
+    .replaceAll('%3F', '?')
+
+    .replaceAll('%5B', '[')
+    .replaceAll('%5C', '\\')
+    .replaceAll('%5D', ']')
+    .replaceAll('%5E', '^')
+
+    .replaceAll('%60', '`')
+
+    .replaceAll('%7B', '{')
+    .replaceAll('%7C', '|')
+    .replaceAll('%7D', '}')
+    .replaceAll('%7E', '~');
+}
 getApi().then(json => {
   const $ = q => document.querySelector(q);
   if (window.type)
