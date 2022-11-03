5deca7f6cde64d4b90803e0a15e44e83e535ef82
AdrianCraft07 <proadrian684@gmail.com>
2022-11-02 23:30:47 -0600

    jun update

--- js/api/junsp13.js
+++ js/api/junsp13.js
@@ -13,27 +13,46 @@ document.querySelector('head').innerHTML += `<style>
     width: 100%;
   }
 }
-</style>`
+</style>`;
 
-console.log("Aga-Api connected")
-window.onload = async function(){
-  let api = (await fetch('https://aga-db.herokuapp.com/api?type=JunSP13').then(r=>r.json())).map(addon => {
-    addon.URL = '';
+console.log('Aga-Api connected');
+const domain = 'https://agacraft.ga'
+window.onload = async function () {
+  let api = (
+    await fetch('https://aga-db.herokuapp.com/api?type=JunSP13').then(r =>
+      r.json()
+    )
+  )
+    .filter(data => !(data.publish === false || data['jun-publish'] === false))
+    .map(addon => {
+      addon.URL = '';
 
-    addon.URL += `https://agacraft.ga/JunSP13?content=${addon.name.replaceAll(' ', '-')}`;
-    addon.icon ||= 'https://adriancraft07.github.io/src/img/proximamente.png';
-    addon.mode = addon.url ? 'Publico' : 'Privado'
-    return addon
-  }).filter(data=>!(data.publish === false || data['jun-publish'] === false));
-  if(location.pathname==='/' || location.pathname==='/F:/JunSP13/index.html')api = [api[0]].map(data=>`<div class="col-lg-4 izquierda">
+      addon.URL += `${domain}/JunSP13?content=${addon.name.replaceAll(
+        ' ',
+        '-'
+      )}`;
+      addon.icon ||= '/src/img/proximamente.png';
+      addon.mode = addon.url ? 'Publico' : 'Privado';
+      addon.icon = domain + addon.icon;
+      return addon;
+    });
+  if (
+    location.pathname === '/' ||
+    location.pathname === '/F:/JunSP13/index.html'
+  )
+    api = [api[0]].map(
+      data => `<div class="col-lg-4 izquierda">
 									  <center><img src="${data.icon}" class="espacio-img" width="128px"></center>
 									</div><div class="col-lg-8 derecha">
 									  <center><h4 class="text-ee"><b>${data.name} (${data.mode})</b></h4>
                                       <p><b>Creador:</b><span class="text-warning2"> AdrianCraft (Aga)</span></p>
                                       <p><b>Descripcion:</b><span class="text-warning2"> ${data.description}</span></p>
 									  <a href="${data.URL}" target="_blank"><button class="btn btn-outline-primary btn-block"><i class="fa fa-eye"></i> Ver ${data.name}</button></a></center>
-								    </div>`)
-  else api = api.map(data => `
+								    </div>`
+    );
+  else
+    api = api.map(
+      data => `
       <div class="card shadow mb-4 card-aga">
           <div class="card-body">
               <div class="col-lg-4 izquierda">
@@ -52,7 +71,7 @@ window.onload = async function(){
               </div>
           </div>
       </div>`
-  )
+    );
   document.querySelector('#data-aga').innerHTML += api.join('');
   return api;
-}
+};
