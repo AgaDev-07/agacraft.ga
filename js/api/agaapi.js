b14aa7b08b2c42eec918c89cc2d02b5b38a51df7
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2022-12-01 19:23:21 -0600

    Update agaapi.js

--- js/api/agaapi.js
+++ js/api/agaapi.js
@@ -8,14 +8,13 @@ function request(url) {
 
 const apiDomain = 'agaapi.webredirect.org:3000';
 async function loadApi(query) {
-  /*
-
+  
     return await fetch(
       `https://${apiDomain}/api?${Object.entries(query)
         .map(([key, value]) => `${key}=${value}`)
         .join('&')}`
     ).then(r => r.json());
-
+  /*
   */
   let { type, content, search, version, index, format } = query;
   const data = await request(
